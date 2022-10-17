b5bfaf11f5fa1b8524bc136f8a9dbbd684c2cd88
dady_ocat@hotmail.com <dady_ocat@hotmail.com>
2022-10-17 12:33:25 +0700

    เช็คค่าว่าง

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 require('condb.php');
 
-if (isset($_POST['name_s'])){
-    $name = $_POST['name_s'];
-    $sql2 = "INSERT INTO user (name) VALUES (?)";
-    $sm  = $conn->prepare($sql2);
-    $sm->bind_param('s',$name);
-    $sm->execute();    
+if (isset($_POST['name_s']) ){
+    if (!empty($_POST['name_s'])){
+        $name = $_POST['name_s'];
+        $sql2 = "INSERT INTO user (name) VALUES (?)";
+        $sm  = $conn->prepare($sql2);
+        $sm->bind_param('s',$name);
+        $sm->execute();
+    }
+    
 }
 
 
